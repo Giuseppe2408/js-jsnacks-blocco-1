6897c206bc6efeee4bc92eb59d836cf10253fc3f
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-05-10 16:40:53 +0200

    struttura

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const array1 = ['parola1', 'parola2', 'parola3', 'parola4', 'parola5'];
 const array2 = ['arr1', 'arr2'];
 const li = document.createElement('li');
-const createUl = document.createElement('ul')
+const createUl = document.createElement('ulidspecial')
 const ul = document.querySelector('ul').appendChild(li);
 const ulChild = document.querySelector('li').appendChild(createUl);
 const liChild = document.querySelector('ul').appendChild(li);
